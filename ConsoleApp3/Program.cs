0aaafe6b7b6de81d975b343e5018e2cd993df5b5
user <user@WINDOWS-HCO0PVU>
2022-05-27 15:52:09 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class _List
+    {
+        private const int _defaultCapacity = 4;
+        public int[] _items;
+        private int _size;
+        private int _version;
+        static readonly int[] _emptyArray = new int[0];
+        public _List()
+        {
+            _items = _emptyArray;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentException("Index was not out of bounds");
+                }
+                return _items[index];
+            }
+            set
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentException("Index was not out of bounds");
+                }
+                _items[index] = value;
+                _version++;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _items.Length;
+            }
+            set
+            {
+                if (value < _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        int[] newItems = new int[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+        public void Add(int item)
+        {
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            _items[_size++] = item;
+            _version++;
+        }
+        public void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                if ((uint)newCapacity > 0X7FEFFFFF)
+                    newCapacity = 0X7FEFFFFF;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+        public class Enumerator : IEnumerator
+        {
+            private _List list;
+            private int index;
+            private int version;
+            private int current;
+            public Enumerator(_List list)
+            {
+                this.list = list;
+                index = 0;
+                current = default(int);
+            }
+            public bool MoveNext()
+            {
+                _List localList = list;
+                if ((uint)index < (uint)localList._size)
+                {
+                    current = localList._items[index];
+                    index++;
+                    return true;
+                }
+                return MoveNextRare();
+            }
+            private bool MoveNextRare()
+            {
+                index = list._size + 1;
+                current = default(int);
+                return false;
+            }
+            public object Current
+            {
+                get
+                {
+                    if (index == 0 || index == list._size + 1)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return current;
+                }
+            }
+            public void Reset()
+            {
+                if (version != list._version)
+                {
+                    throw new NullReferenceException();
+                }
+
+                index = 0;
+                current = default;
+            }
+            #region Homework
+            public int[] Clear()
+            {
+                return _emptyArray;
+            }
+
+        }
+        public bool Contains(uint item)
+        {
+            foreach (uint value in _items)
+            {
+                if (value == item)
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+    }
+
+    public interface IEnumerator
+    {
+    }
+    #endregion
+}
+
+
---
+++ ConsoleApp3/_ListBase.cs
@@ -0,0 +1,99 @@
+﻿namespace ConsoleApp3
+{
+    public class _ListBase
+    {
+        private const int _defaultCapacity = 4;
+        static readonly int[] _emptyArray = new int[0];
+        public int[] _items;
+        private int _size;
+        private int _version;
+        public int this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentException("Index was not out of bounds");
+                }
+                return _items[index];
+            }
+            set
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentException("Index was not out of bounds");
+                }
+                _items[index] = value;
+                _version++;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _items.Length;
+            }
+            set
+            {
+                if (value < _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        int[] newItems = new int[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+        public void Add(int item)
+        {
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            _items[_size++] = item;
+            _version++;
+        }
+        public bool Contains(uint item)
+        {
+            foreach (uint value in _items)
+            {
+                if (value == item)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                if ((uint)newCapacity > 0X7FEFFFFF)
+                    newCapacity = 0X7FEFFFFF;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+    }
+}
